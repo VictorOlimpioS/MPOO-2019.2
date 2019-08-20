447dc5623bd89c71f1cfc144ef2e157a69e03d00
victor-olimpio <victor.olimpio@gmail.com>
2019-08-20 17:35:27 -0300

    clean

--- src/mpoo/Application.java
+++ src/mpoo/Application.java
@@ -3,11 +3,15 @@ package mpoo;
 
 public class Application {
 	public static void main(String[] args) {
-		Helloworld helloworld = new Helloworld();
-		helloworld.setNome("Victor");
-		helloworld.imprimir();
-		helloworld.setNome("Débora");
-		helloworld.imprimir();
+		Helloworld helloworld1 = new Helloworld();
+		helloworld1.setNome("Victor");
+		helloworld1.imprimir();
+
+		helloworld1.setNome("Victor Olimpio");
+		helloworld1.imprimir();
+
+		Helloworld helloworld2 = new Helloworld();
+		helloworld2.imprimir();
 		
 	}
 
--- src/mpoo/Helloworld.java
+++ src/mpoo/Helloworld.java
@@ -1,5 +1,9 @@
 package mpoo;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Helloworld {
 	private String nome;
 
@@ -10,8 +14,11 @@ public class Helloworld {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public void imprimir() {
-		System.out.println("Olá " + this.getNome() + ". Você acabou de fazer seu primeiro Hello World em Java. Parabéns.");
+		Date horaAtual = new Date();
+		SimpleDateFormat horaFormat = new SimpleDateFormat("HH:mm:ss");
+		System.out.println( horaFormat.format(horaAtual.getTime()) + " - Olá " + this.getNome() + ". Você acabou de fazer seu primeiro Hello World em Java. Parabéns.");
 	}
 
 }
