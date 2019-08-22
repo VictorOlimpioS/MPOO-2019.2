4424af8c0ce478391b3a4ebdfe45e0d16d24d6c7
victor-olimpio <victor.olimpio@gmail.com>
2019-08-22 09:40:27 -0300

    clean

--- src/mpoo/Application.java
+++ src/mpoo/Application.java
@@ -3,16 +3,7 @@ package mpoo;
 
 public class Application {
 	public static void main(String[] args) {
-		Helloworld helloworld1 = new Helloworld();
-		helloworld1.setNome("Victor");
-		helloworld1.imprimir();
 
-		helloworld1.setNome("Victor Olimpio");
-		helloworld1.imprimir();
-
-		Helloworld helloworld2 = new Helloworld();
-		helloworld2.imprimir();
-		
 	}
 
 }
